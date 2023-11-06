fc4e6e70b70e7ba67a02a6dd96be08db5f3f3890
Frederick <120729769+sharpartzgh@users.noreply.github.com>
2023-11-06 22:02:55 +0000

    my first React Application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>My name is Frederick Asiamah,<br /> this is my first React application</p>
+    </>
+  )
+}
+
+export default App
