47ff2ef77a4061e34995af599840b286e9d26b84
Frederick <120729769+sharpartzgh@users.noreply.github.com>
2023-11-12 23:33:07 +0000

    Assignment 3 solution on React Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 
 import './App.css'
+import Button from './components/Button'
 
-function App() {
-  let name = "Frederick Asiamah Apenteng";
-  let education = ["Certified Graphic Designer,"," ", "currently studying Frontend Development at Azubi Africa"]
-  let skills = ["Graphic Design, ",
-    "HTML, ",
-    "CSS, ",
-    "JavaScript, ",
-    "React and ","Pencil Portrait Art,"]
-    const pBackground = 'Lead Graphic Designer at Kabnedy Enterprise';
+// function App() {
+//   let name = "Frederick Asiamah Apenteng";
+//   let education = ["Certified Graphic Designer,"," ", "currently studying Frontend Development at Azubi Africa"]
+//   let skills = ["Graphic Design, ",
+//     "HTML, ",
+//     "CSS, ",
+//     "JavaScript, ",
+//     "React and ","Pencil Portrait Art,"]
+//     const pBackground = 'Lead Graphic Designer at Kabnedy Enterprise';
+//   return (
+//     <>
+//       <h1>Hello World</h1>
+//       <p>I am thrilled to connect with you as a passionate visual artist and graphic designer <br />merging creativity with Front-end Development to create  <br />captivating digital experiences. I am {name}, <br />a {education}</p>
+//       <h2>My Skills</h2>
+//       <ul>
+//         <li>{skills}</li>
+//       </ul>
+//       <h2>Professional Background</h2>
+//       <p>{pBackground}</p>
+//       <button>Submit</button>
+//     </>
+//   )
+// }
+function App (){
+
   return (
     <>
-      <h1>Hello World</h1>
-      <p>I am thrilled to connect with you as a passionate visual artist and graphic designer <br />merging creativity with Front-end Development to create  <br />captivating digital experiences. I am {name}, <br />a {education}</p>
-      <h2>My Skills</h2>
-      <ul>
-        <li>{skills}</li>
-      </ul>
-      <h2>Professional Background</h2>
-      <p>{pBackground}</p>
+    <Button name='Login'/>
+    <hr />
+    <Button name='Signin'/>
     </>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button(props) {
+  return (
+    <>
+      <button className='btn'>{props.name}</button>
+    </>
+  )
+}
+
+export default Button
